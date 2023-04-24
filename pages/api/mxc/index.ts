5d41eb6c1a7f6ad285f05d05413a8826238b1028
Caltara <calt13@proton.me>
2023-04-24 18:10:04 +0800

    First commit

---
+++ pages/api/mxc/index.ts
@@ -0,0 +1,20 @@
+import type { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  runtime: "edge",
+  api: {
+    responseLimit: "16mb",
+  },
+};
+
+export default async function handler(req: NextRequest) {
+  const reqUrl = new URL(req.url);
+  const mxcUri = reqUrl.searchParams.get("uri");
+  if (!mxcUri) {
+    return new Response("mxc URI should not be blank", { status: 400 });
+  }
+  const serverUrl = process.env.MATRIX_SERVER_URL;
+  const downloadUrl = `${serverUrl}/download/${mxcUri.substring(6)}`;
+
+  return await fetch(downloadUrl);
+}
